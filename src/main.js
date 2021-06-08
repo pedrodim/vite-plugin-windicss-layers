a06e4321e05095faca7f63b89b56f0e1e0634d6e
Pedro Di Martino <dimartino.pedro@gmail.com>
2021-06-08 16:58:57 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import 'virtual:windi.css'
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div class="custom-2 lg:custom-1">Custom</div>
+`
---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import WindiCSS from 'vite-plugin-windicss'
+
+export default {
+  server: {
+    host: "::",
+  },
+  plugins: [
+    WindiCSS(),
+  ],
+}
